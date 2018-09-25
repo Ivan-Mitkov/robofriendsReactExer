887769dcbf24d5fdf6043506032e3d6f03edee9c
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-09-25 08:56:38 +0300

    add redux and initial reducer

---
+++ src/reducers.js
@@ -0,0 +1,17 @@
+import { CHANGE_SEARCH_FIELD } from './constants'
+
+const initialState = {
+    searchField=''
+}
+
+export const searchRobots = (state = initialState, action = {}) => {
+    switch (action.type) {
+        case CHANGE_SEARCH_FIELD:
+        //return new state wich will have everthing in the state {} plus it will update (state)
+        //with new searchField:action.payload
+            return Object.assign({}, state, { searchField: action.payload });
+        //pure function always must return something    
+        default:
+        return state;
+    }
+}
