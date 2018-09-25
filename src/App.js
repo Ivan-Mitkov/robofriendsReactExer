8b4b3031e61be1bee698bbfe768eede6ff6346a4
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-09-25 07:28:28 +0300

    add scrollable component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Cardlist from './Cardlist';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './app.css';
 
 class App extends Component {
@@ -13,13 +14,13 @@ class App extends Component {
     }
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response=>{
+            .then(response => {
                 return response.json();
             })
-            .then(users=>{
-                this.setState({robots:users})
+            .then(users => {
+                this.setState({ robots: users })
             });
-      
+
     }
 
     //this of event is in input so in order to use this of App we are using => function
@@ -33,21 +34,24 @@ class App extends Component {
             // this.state.searchfield.setState(event.target.value);
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
-        if(this.state.robots.length===0){
+        if (this.state.robots.length === 0) {
             return <h1>Loading....</h1>
-        }else{
+        } else {
             return (
                 <div className='tc'>
                     <h1 className='f2' >
                         Robofriends
                     </h1>
                     <SearchBox searchChange={this.onSearchChange} />
-                    <Cardlist robots={filteredRobots} />
+                    <Scroll>
+                        <Cardlist robots={filteredRobots} />
+                    </Scroll>
+
                 </div>
-    
+
             );
         }
-       
+
     }
 
 }
